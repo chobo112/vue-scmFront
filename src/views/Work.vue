c95adae089e70e372b8a1f2cee69d065e7e59c8c
hilim9 <dlagydls1212@naver.com>
2024-09-06 16:39:35 +0900

    발주현황 완료

---
+++ src/views/Work.vue
@@ -0,0 +1,17 @@
+<template>
+    <ContextBox>발주현황</ContextBox>
+    <WorkSearch />
+    <WorkMain />
+</template>
+
+<script>
+import ContextBox from "@/components/common/ContextBox.vue";
+import WorkMain from "@/components/page/Work/WorkMain.vue";
+import WorkSearch from "@/components/page/Work/WorkSearch.vue";
+import WorkModal from "@/components/page/Work/WorkModal.vue";
+
+export default {
+    name: "Work",
+    components: { ContextBox, WorkMain, WorkSearch, WorkModal }
+};
+</script>
