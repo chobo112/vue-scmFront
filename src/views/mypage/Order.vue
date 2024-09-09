6fcefe223be43d4dddfa4c6781fb41019459e6e6
verde1105 <verde1105@lunaflo@naver.com>
2024-09-09 09:43:16 +0900

    스타일오류수정

---
+++ src/views/mypage/Order.vue
@@ -0,0 +1,15 @@
+<template>
+    <ContextBox>구매내역</ContextBox>
+    <OrderMain></OrderMain>
+    
+</template>
+
+<script setup>
+import ContextBox from '@/components/common/ContextBox.vue';
+import OrderMain from '@/components/page/mypage/order/OrderMain.vue';
+
+
+
+</script>
+
+<style></style>
